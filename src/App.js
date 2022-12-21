6961d6d594de9f48a89148cff1a3d16fde2433c6
Phinjock Sherpa <phinjock@gmail.com>
2022-12-21 13:58:52 -0800

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <ExpenseItem></ExpenseItem>;
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2022, 12, 21),
+    },
+    {
+      id: "e2",
+      title: "Car finance",
+      amount: 294.67,
+      date: new Date(2022, 12, 21),
+    },
+  ];
+  return;
+  <div>
+    <ExpenseItem
+      title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date}
+    ></ExpenseItem>
+
+    <ExpenseItem
+      title={expenses[1].title}
+      amount={expenses[1].amount}
+      date={expenses[1].date}
+    ></ExpenseItem>
+  </div>;
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2022, 12, 20);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div> March 28th 2021</div>
+      <div> {expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Inusrance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );

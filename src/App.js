576c932e7328c551d950787a2544190b77cc6611
Phinjock Sherpa <phinjock@gmail.com>
2022-12-21 15:49:00 -0800

    applying props to reuse component data

--- src/App.js
+++ src/App.js
@@ -15,20 +15,21 @@ function App() {
       date: new Date(2022, 12, 21),
     },
   ];
-  return;
-  <div>
-    <ExpenseItem
-      title={expenses[0].title}
-      amount={expenses[0].amount}
-      date={expenses[0].date}
-    ></ExpenseItem>
+  return (
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
 
-    <ExpenseItem
-      title={expenses[1].title}
-      amount={expenses[1].amount}
-      date={expenses[1].date}
-    ></ExpenseItem>
-  </div>;
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+    </div>
+  );
 }
 
 export default App;

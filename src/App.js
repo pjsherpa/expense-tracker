a898d2cc3b43b5e52912616ad8f03ab746fa6ea6
Phinjock Sherpa <phinjock@gmail.com>
2022-12-23 12:54:37 -0800

    created own card wrapper and used props.children to reformat styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 180.67,
+      date: new Date(2022, 12, 21),
+    },
+    {
+      id: "e2",
+      title: "Car finance",
+      amount: 494.67,
+      date: new Date(2022, 12, 21),
+    },
+    {
+      id: "e3",
+      title: "Dentist finance",
+      amount: 494.67,
+      date: new Date(2022, 12, 21),
+    },
+  ];
   return (
     <div>
-      <ExpenseItem />
+      <Expenses items={expenses} />
     </div>
   );
 }

f5cb0c65fcb308fe5a0a483b69270ec30d72121a
Phinjock Sherpa <phinjock@gmail.com>
2022-12-22 21:00:40 -0800

    added extra expenses

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 180.67,
-      date: new Date(2022, 12, 21),
-    },
-    {
-      id: "e2",
-      title: "Car finance",
-      amount: 494.67,
-      date: new Date(2022, 12, 21),
-    },
-  ];
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
+      <ExpenseItem />
     </div>
   );
 }

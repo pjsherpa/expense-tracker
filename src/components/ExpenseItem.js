7d7312d2214c0e4c5616891d19a3a2b5ec0124e1
Phinjock Sherpa <phinjock@gmail.com>
2023-01-10 17:31:23 -0800

    add new variable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,14 +3,16 @@ import "./ExpenseItem.css";
 import Card from "./Card";
 
 const ExpenseItem = (props) => {
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log("Clicked!!!");
+    title = "updated";
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>

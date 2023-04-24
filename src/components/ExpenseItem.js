6a59197edde5f8901c673711f38a2e009d9f5d9e
Phinjock Sherpa <phinjock@gmail.com>
2023-04-24 13:15:18 -0700

    update for title button included

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,21 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "./Card";
+import React, { useState } from "react";
 
 const ExpenseItem = (props) => {
   let title = props.title;
 
+  const [titles, setTitles] = useState(title);
+
   const clickHandler = () => {
-    title = "updated";
+    setTitles("Updated!");
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{titles}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
